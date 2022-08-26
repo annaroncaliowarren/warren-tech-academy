a7883384289aa9c975a43c1fc0ccdb860a75f312
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-08-26 14:39:41 -0300

    add entity framework and migrations

---
+++ Warren/Data/Context/WarrenContext.cs
@@ -0,0 +1,23 @@
+﻿using Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Context
+{
+    public class WarrenContext : DbContext // classe pronta do entity framework para conectar no banco de dados
+    {
+        #region Props
+        public DbSet<Animal> Animal { get; set; } // para conectar o model com tabela do banco / cria tabela
+
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) // override de um método pronto do entity framework
+        {
+            optionsBuilder.UseNpgsql("Host=localhost; Port=5435; Database=postgres; UserId=postgres; Password=123456"); // info para conectar no banco de dados
+        }
+    }
+}
